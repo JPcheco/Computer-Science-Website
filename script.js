a6300385e87e49cf053ef5998e350ae4ec124808
JPcheco <124377479+JPcheco@users.noreply.github.com>
2023-03-02 17:31:39 -0500

    "scroll-to-top" btn
    Added a "scroll-up-btn" to smoothly bring you back up to the top of the screen
    
    also cleaned up some of the html

--- script.js
+++ script.js
@@ -5,11 +5,30 @@ navBarToggle.addEventListener("click", function(){
     mainNav.classList.toggle("active");
 });
 
+// Scroll up button
+    let scrollButton = document.getElementById("scrollbtn");
+
+    window.addEventListener("scroll", () => {
+        if(window.pageYOffset > 25){
+            scrollButton.classList.add("active");
+        }else{
+            scrollButton.classList.remove("active");
+        }
+    });
+
+    // this is how to scoll to the top
+    scrollButton.addEventListener("click", function(){
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "smooth"
+        });
+    });
 
 // FAQ collapse inner workings
-const toggles = document.querySelectorAll('.faq-toggle')
-toggles.forEach(toggle => {
-    toggle.addEventListener('click', () => {
-        toggle.parentNode.classList.toggle('active')
+    const toggles = document.querySelectorAll('.faq-toggle')
+    toggles.forEach(toggle => {
+        toggle.addEventListener('click', () => {
+            toggle.parentNode.classList.toggle('active')
+        })
     })
-})
