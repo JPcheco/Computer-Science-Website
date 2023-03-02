f1eca759e609a43f37912d08873d9701f7464abe
JPcheco <124377479+JPcheco@users.noreply.github.com>
2023-03-02 14:06:41 -0500

    Changing FAQ style
    Toggle system now in place on traditional Q&A
    
    Also adjusted the footer (removed Names)

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ let navBarToggle = document.getElementById("hb-btn");
 navBarToggle.addEventListener("click", function(){
     mainNav.classList.toggle("active");
 });
+
+
+// FAQ collapse inner workings
+const toggles = document.querySelectorAll('.faq-toggle')
+toggles.forEach(toggle => {
+    toggle.addEventListener('click', () => {
+        toggle.parentNode.classList.toggle('active')
+    })
+})
