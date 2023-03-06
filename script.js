1265b14dfb24d77c5f079ebc66596f6d78524cd6
Rybeardawg1 <104077296+Rybeardawg1@users.noreply.github.com>
2023-03-06 14:33:48 -0500

    Font, Scrollbar, Course Search
    The HTML for the search is all set, the CSS is almost there, and the JS needs a lot of work. The scroll is now a rounded rectangle. The font has been changed to Stonehill's standard font but I'm not totally sure if its taking effect on everything. It's def taking effect on the navbar.

--- script.js
+++ script.js
@@ -1,27 +1,48 @@
 // Scroll up button
-    let scrollButton = document.getElementById("scrollbtn");
+let scrollButton = document.getElementById("scrollbtn");
 
-    window.addEventListener("scroll", () => {
-        if(window.pageYOffset > 25){
-            scrollButton.classList.add("active");
-        }else{
-            scrollButton.classList.remove("active");
-        }
-    });
+window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 25) {
+        scrollButton.classList.add("active");
+    } else {
+        scrollButton.classList.remove("active");
+    }
+});
 
-    // this is how to scoll to the top
-    scrollButton.addEventListener("click", function(){
-        window.scrollTo({
-            top: 0,
-            left: 0,
-            behavior: "smooth"
-        });
+// this is how to scoll to the top
+scrollButton.addEventListener("click", function () {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
     });
+});
 
 // FAQ collapse inner workings
-    const toggles = document.querySelectorAll('.faq-toggle')
-    toggles.forEach(toggle => {
-        toggle.addEventListener('click', () => {
-            toggle.parentNode.classList.toggle('active')
-        })
+const toggles = document.querySelectorAll('.faq-toggle')
+toggles.forEach(toggle => {
+    toggle.addEventListener('click', () => {
+        toggle.parentNode.classList.toggle('active')
     })
+})
+
+
+function myFunction() {
+    // Declare variables
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById('myInput');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName('br');
+
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
